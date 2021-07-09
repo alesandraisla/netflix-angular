d133b8fe9f077301ad7f9005ce1ab795a7a03b7d
alesandra mempis <alesandramempis@gmail.com>
2021-07-09 11:41:28 -0300

    Ajuste na interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "home",
+  component: HomeComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
