cf7825e0fdf5f12ee0410eb21fe6b22206f0075d
alesandra mempis <alesandramempis@gmail.com>
2021-07-12 22:11:39 -0300

    Criação da página dos perfis

--- src/app/components/profiles/manage/manage.component.ts
+++ src/app/components/profiles/manage/manage.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageComponent implements OnInit {
 
+  users = [
+    {
+      img: 'assets/img/tigre.png',
+      name: 'Lara Croft'
+    },
+    {
+      img: 'assets/img/boneco.png',
+      name: 'Ichigo Kurosaki'
+    },
+    {
+      img: 'assets/img/catsz.png',
+      name: 'Final Fantasy'
+    },
+    {
+      img: 'assets/img/boneca.png',
+      name: 'Alesandra & Zoe'
+    },
+    {
+      img: 'assets/img/smile.png',
+      name: 'Naruto Uzumaki'
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
